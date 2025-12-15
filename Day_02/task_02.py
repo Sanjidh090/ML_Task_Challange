4fdebe1edf4489a50a51c0700a0fdd34e0438ff3
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2025-12-16 01:02:01 +0600

    Update task_02.py

--- Day_02/task_02.py
+++ Day_02/task_02.py
@@ -1,5 +1,5 @@
 x  = 0.2 + 0.1
-if x == 0.3:
-  print("True")
-else:
-  print("False")
+#False if compared simply 
+print("True") if  x == 0.3  else  print("False")
+#Fix
+print("True") if  round(x,1) == 0.3  else  print("False")
