60c3035eae9a667fd7cdd1c6090fbfc06ab006de
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2025-12-15 23:55:04 +0600

    Update task_01.py

--- Day_02/task_01.py
+++ Day_02/task_01.py
@@ -1,1 +1,5 @@
-
+user = None 
+if user != None and user.role == 'admin' : 
+    print(f"User is {user} and user's role is {user.role}")
+else: 
+    print("User is None or does not have admin access")
