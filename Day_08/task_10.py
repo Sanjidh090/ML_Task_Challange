18ff000d5fc69987c5c14870cd8f0564baea99ad
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-06 23:32:29 +0600

    Update task_10.py

--- Day_08/task_10.py
+++ Day_08/task_10.py
@@ -1,1 +1,13 @@
-.
+import time
+x = [1,2,5,6,8,9,7,10,4,5]
+pos = 4
+def slice_list(source,k):
+    s = time.time()
+    new_list = []
+    for i in range(0,k):
+        new_list.append(source[i])
+    e = time.time()
+    print(f"Time taken: {e - s} seconds")
+    return new_list
+
+print(slice_list(x,pos))  # [1, 2, 3]
