25271f3979ab9542d1d504da0a8b879746a077a8
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-06 23:41:34 +0600

    Update task_07.py

--- Day_08/task_07.py
+++ Day_08/task_07.py
@@ -1,5 +1,4 @@
 import random
-
 x = [random.randint(0, 10000) for _ in range(1000)]
 def sort_list(x = []):
     import time
@@ -9,3 +8,4 @@ def sort_list(x = []):
     print(f"Sorting took: {end - start} seconds")
 if __name__ == "__main__": 
     sort_list(x)
+
