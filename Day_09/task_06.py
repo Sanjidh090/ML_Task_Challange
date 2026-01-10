0a123805c946fe6d343706bdfbcf0b895ecfdf87
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-10 23:17:55 +0600

    Update task_06.py

--- Day_09/task_06.py
+++ Day_09/task_06.py
@@ -1,1 +1,10 @@
+def square(x):
+    yield x * x
 
+def filter_even(numbers):
+    for number in numbers:  # iterate over the input
+        if number % 2 == 0:
+            yield number
+filtered_numbers = filter_even(square(6))
+for i in filtered_numbers:
+    print(i)
