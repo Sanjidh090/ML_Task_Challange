9c9f9e6b4a1fca6b29e8a7618d464ccd3bbd1483
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-10 22:59:40 +0600

    Update task_04.py

--- Day_09/task_04.py
+++ Day_09/task_04.py
@@ -1,1 +1,12 @@
-
+def gen():
+    yield 1
+    yield 2
+g = gen()
+try:
+    print(next(g))
+    print(next(g))
+    print(next(g))
+except StopIteration:
+    print("No more values to yield.")
+    g = gen()  # Reinitialize the generator
+    print(next(g))
