2eaff4c4a3c0c1b5fa9519cd8b4bb047945a804b
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-11 19:12:10 +0600

    Update task_01.py

--- Day_10/task_01.py
+++ Day_10/task_01.py
@@ -1,1 +1,12 @@
+def wrapper(func):
+    def inner():
+        print("Before")
+        func()
+        print("After")
+    return inner
 
+def old_func():
+    print("Hello")
+
+new_func = wrapper(old_func)
+new_func()
