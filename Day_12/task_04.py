2e6b82c77eedd690fb7aae2f9d1379c3329e56de
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-14 05:02:33 +0600

    Update task_04.py

--- Day_12/task_04.py
+++ Day_12/task_04.py
@@ -1,1 +1,15 @@
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.__password = password   # private (name-mangled)
 
+    def check_password(self, password):
+        return self.__password == password
+        
+user = User("sanjid", "1234")
+
+print(user.username)
+# sanjid
+
+print(user.check_password("1234"))
+# True 
