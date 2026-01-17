18915aef6a85498233238cc19282abf376042689
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-17 17:27:56 +0600

    Add User and Admin classes with basic functionality

--- Day_12/task_08.py
+++ Day_12/task_08.py
@@ -1,1 +1,20 @@
+class User:
+    def __init__(self, username):
+        self.username = username
+    def info(self):
+        return f"{self.username} is Added in user information."
+        
+class Admin(User):
+    def __init__(self, username, Access):
+        super().__init__(username)
+        self.access = Access     
+    def delete_db(self):
+        return f"Admin {self.username} deleted the database and has access to {self.access}!"
 
+user = User("Sanjid")
+print(user.info())                    
+
+admin = Admin("Hasan", True)
+print(admin.delete_db())                
+print(admin.username)                      
+print(admin.access)                    
