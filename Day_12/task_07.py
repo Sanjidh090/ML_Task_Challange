66f4673eb803796feef337195cf5dc713540ff79
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-17 17:24:41 +0600

    Update task_07.py

--- Day_12/task_07.py
+++ Day_12/task_07.py
@@ -1,1 +1,16 @@
+class User:
+    def __init__(self, username):
+        self.username = username
+    def info(self):
+        return f"{self.username} is Added Here"   
+      
+class Admin(User):
+    def __init__(self, username):
+        super().__init__(username)
+    def delete_all(self):
+        return f"Admin {self.username} deleted "
 
+user = User("Sanjid")
+print(user.info())                    
+admin = Admin("Hasan")
+print(admin.delete_all())         
