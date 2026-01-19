41c59de4bcefca74a963b154593d9d8387e642a8
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-19 23:21:16 +0600

    Update task_08.py

--- Day_13/task_08.py
+++ Day_13/task_08.py
@@ -1,1 +1,14 @@
+import os
+
+try:
+    folder_path = "/Download/sample_data"  # Example folder path
+    file_name = "README.md"             # Example file name
+
+    # Join folder and file into a full path
+    full_path = os.path.join(folder_path, file_name)
+
+    print("Full path:", full_path)
+
+except Exception as e:
+    print("Error:", e)
 
